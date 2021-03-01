0a2791aa869d17ea619aafaadc7d52b1356d1c09
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-03-01 11:41:09 -0800

    Remove duplicate import of css.

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import './css/styles.css';
 import Triangle from './triangle.js';
 
 $(document).ready(function() {
