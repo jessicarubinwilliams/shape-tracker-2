573239ce01da20edc42202b7269f945079553ef0
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-03-02 11:22:53 -0800

    Update relative path of import statements & import Rectangle.

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Triangle from './triangle.js';
+import Triangle from './js/triangle.js';
+import Rectangle from './js/rectangle.js'
 
 $(document).ready(function() {
   $('#triangle-checker-form').submit(function(event) {
