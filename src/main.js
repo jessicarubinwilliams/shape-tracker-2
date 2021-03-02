e281291b12f990c2c0bcf015b872f5e13c659662
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-03-02 11:51:05 -0800

    Add UIL for determining area of user's rectangle.

--- src/main.js
+++ src/main.js
@@ -15,4 +15,12 @@ $(document).ready(function() {
     const response = triangle.checkType();
     $('#response').append("<p>" + response + "</p>");
   });
+  $('#rectangle-area-form').submit(function(event) {
+    event.preventDefault();
+    const length1 = parseInt($('#rect-length1').val());
+    const length2 = parseInt($('#rect-length2').val());
+    const rectangle = new Rectangle(length1, length2);
+    const response = rectangle.getArea();
+    $('#response2').append(`<p> The area of the rectangle is ${response}.</p>`);
+  });
 });
