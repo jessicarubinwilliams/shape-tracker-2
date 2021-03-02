d6dd1f42c075ca12fed2f08aca32d04afef20886
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-03-02 13:22:03 -0800

    Update triangle submit fn w/.parseInt and template literal.

--- src/main.js
+++ src/main.js
@@ -8,12 +8,12 @@ import Rectangle from './js/rectangle.js'
 $(document).ready(function() {
   $('#triangle-checker-form').submit(function(event) {
     event.preventDefault();
-    const length1 = $('#length1').val();
-    const length2 = $('#length2').val();
-    const length3 = $('#length3').val();
+    const length1 = parseInt($('#length1').val());
+    const length2 = parseInt($('#length2').val());
+    const length3 = parseInt($('#length3').val());
     const triangle = new Triangle(length1, length2, length3);
     const response = triangle.checkType();
-    $('#response').append("<p>" + response + "</p>");
+    $('#response').append(`<p>${response}</p>`);
   });
   $('#rectangle-area-form').submit(function(event) {
     event.preventDefault();
